7db2af32a544c17d5d2635f86a26be1e772304e2
Alberto Ibarra <albib123@icloud.com>
2022-05-30 12:09:14 -0700

    commit two

---
+++ backend/routes/userRoutes.js
@@ -0,0 +1,12 @@
+const express = require('express')
+const router = express.Router()
+const {RegisterUser, LoginUser, getMe} = require('../controllers/userController')
+
+const {protect} = require('../middleware/authMiddleware')
+
+
+router.post('/', RegisterUser)
+router.post('/login', LoginUser)
+router.get('/me', protect, getMe)
+
+module.exports = router
